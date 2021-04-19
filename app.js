42f9ae67d969bbf90432299ab7c72919cdd80f08
chrisbremmer <c_bremmer@outlook.com>
2021-04-19 09:22:00 -0700

    Added express middleware

--- app.js
+++ app.js
@@ -5,10 +5,23 @@ require('dotenv/config')
 
 const api = process.env.API_URL
 
-app.get(api + '/', (req, res) => {
-  res.send('hello API!')
+// Middleware
+app.use(express.json());
+
+app.get(`${api}/products`, (req, res) => {
+  const product = {
+    id: 1,
+    name: 'hair dresser',
+    image: 'some_url'
+  }
+  res.send(product)
+})
+
+app.post(`${api}/products`, (req, res) => {
+  const newProduct = req.body;
+  console.log(newProduct);
+  res.send(newProduct)
 })
 app.listen(3000, () => {
-  console.log(api)
   console.log(`Server is running http://localhost:3000`)
 })
