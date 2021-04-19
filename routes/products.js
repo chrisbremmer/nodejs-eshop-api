1a8cda9402186a6b8d323e6826b92c2df8addc2e
chrisbremmer <c_bremmer@outlook.com>
2021-04-19 16:00:12 -0700

    Update a product

--- routes/products.js
+++ routes/products.js
@@ -9,7 +9,7 @@ const express = require('express')
 const router = express.Router()
 
 router.get(`/`, async (req, res) => {
-  const productList = await Product.find().select('name image -_id');
+  const productList = await Product.find().populate('category')
 
   if (!productList) {
     res.status(500).json({
@@ -20,7 +20,7 @@ router.get(`/`, async (req, res) => {
 })
 
 router.get(`/:id`, async (req, res) => {
-  const product = await Product.findById(req.params.id);
+  const product = await Product.findById(req.params.id).populate('category');
 
   if (!product) {
     res.status(500).json({
@@ -48,13 +48,40 @@ router.post(`/`, async (req, res) => {
     countInStock: req.body.countInStock,
     rating: req.body.rating,
     numReviews: req.body.numReviews,
-    isFeatured: req.body.isFeaturede
+    isFeatured: req.body.isFeatured
   })
 
   product = await product.save()
 
   if (!product) {
     return res.status(500).send('The product cannot be created')
+  }
+  res.send(product)
+})
+
+router.put('/:id', async (req, res) => {
+  const category = await Category.findById(req.body.category);
+  if (!category) {
+    return res.status(400).send('Invalid Category')
+  }
+  const product = await Product.findByIdAndUpdate(req.params.id, {
+    name: req.body.name,
+    description: req.body.description,
+    richDescription: req.body.richDescription,
+    image: req.body.image,
+    brand: req.body.brand,
+    price: req.body.price,
+    category: req.body.category,
+    countInStock: req.body.countInStock,
+    rating: req.body.rating,
+    numReviews: req.body.numReviews,
+    isFeatured: req.body.isFeatured
+  }, {
+    new: true,
+    useFindAndModify: false
+  })
+  if (!product) {
+    return res.status(500).send('the product cannot be updated!')
   }
   res.send(product)
 })
