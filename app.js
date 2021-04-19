beb9d2abc9d3b2cbc8c1760f705dcfbabb81f820
chrisbremmer <c_bremmer@outlook.com>
2021-04-19 09:14:50 -0700

    added environment variables

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => {
+require('dotenv/config')
+
+const api = process.env.API_URL
+
+app.get(api + '/', (req, res) => {
   res.send('hello API!')
 })
 app.listen(3000, () => {
+  console.log(api)
   console.log(`Server is running http://localhost:3000`)
 })
