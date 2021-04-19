18ea587bfca6cefcc740e6dd133c24aedea3e9c0
chrisbremmer <c_bremmer@outlook.com>
2021-04-19 13:02:36 -0700

    Get categories and category details

--- routes/categories.js
+++ routes/categories.js
@@ -12,7 +12,18 @@ router.get(`/`, async (req, res) => {
       success: false
     })
   }
-  res.send(categoryList)
+  res.status(200).send(categoryList)
+})
+
+router.get('/:id', async (req, res) => {
+  const category = await Category.findById(req.params.id)
+
+  if (!category) {
+    res.status(500).json({
+      message: 'The category with the given ID was not found.'
+    })
+  }
+  res.status(200).send(category)
 })
 
 router.post('/', async (req, res) => {
