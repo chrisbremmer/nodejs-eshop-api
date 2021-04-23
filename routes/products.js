37b7b07faaf894c94dead4b415cc964e1973b94d
chrisbremmer <c_bremmer@outlook.com>
2021-04-23 12:06:21 -0700

    image upload in put request

--- routes/products.js
+++ routes/products.js
@@ -97,19 +97,33 @@ router.post(`/`, uploadOptions.single('image'), async (req, res) => {
   res.send(product);
 })
 
-router.put('/:id', async (req, res) => {
+router.put('/:id', uploadOptions.single('image'), async (req, res) => {
   if (!mongoose.isValidObjectId(req.params.id)) {
     return res.status(400).send('Invalid Product Id')
   }
   const category = await Category.findById(req.body.category);
   if (!category) return res.status(400).send('Invalid Category')
 
-  const product = await Product.findByIdAndUpdate(
+  const product = await Product.findById(req.params.id);
+  if (!product) return res.status(400).send('Invalid Product')
+
+  const file = req.file
+  let imagepath;
+
+  if (file) {
+    const fileName = file.filename
+    const basePath = `${req.protocol}://${req.get('host')}/public/uploads/`
+    imagepath = `${basePath}${fileName}`
+  } else {
+    imagepath = product.image;
+  }
+
+  const updatedProduct = await Product.findByIdAndUpdate(
     req.params.id, {
       name: req.body.name,
       description: req.body.description,
       richDescription: req.body.richDescription,
-      image: req.body.image,
+      image: imagepath,
       brand: req.body.brand,
       price: req.body.price,
       category: req.body.category,
@@ -122,10 +136,10 @@ router.put('/:id', async (req, res) => {
     }
   )
 
-  if (!product)
+  if (!updatedProduct)
     return res.status(500).send('the product cannot be updated!')
 
-  res.send(product);
+  res.send(updatedProduct);
 })
 
 router.delete('/:id', (req, res) => {
