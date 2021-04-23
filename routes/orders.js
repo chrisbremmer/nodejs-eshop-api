6da81b052e4d269b6519cb4cb63d9ec6c2d1e22b
chrisbremmer <c_bremmer@outlook.com>
2021-04-23 09:33:01 -0700

    calculating total price of one order

--- routes/orders.js
+++ routes/orders.js
@@ -40,17 +40,25 @@ router.get(`/:id`, async (req, res) => {
 })
 
 router.post('/', async (req, res) => {
-  const orderItemsIds = Promise.all(req.body.orderItems.map(async orderItem => {
+  const orderItemsIds = Promise.all(req.body.orderItems.map(async (orderItem) => {
     let newOrderItem = new OrderItem({
       quantity: orderItem.quantity,
       product: orderItem.product
     })
-    newOrderItem = await newOrderItem.save()
 
-    return newOrderItem._id
+    newOrderItem = await newOrderItem.save();
+
+    return newOrderItem._id;
   }))
-  const orderItemsIdsResolved = await orderItemsIds
+  const orderItemsIdsResolved = await orderItemsIds;
 
+  const totalPrices = await Promise.all(orderItemsIdsResolved.map(async (orderItemId) => {
+    const orderItem = await OrderItem.findById(orderItemId).populate('product', 'price');
+    const totalPrice = orderItem.product.price * orderItem.quantity;
+    return totalPrice
+  }))
+
+  const totalPrice = totalPrices.reduce((a, b) => a + b, 0)
 
   let order = new Order({
     orderItems: orderItemsIdsResolved,
@@ -61,7 +69,7 @@ router.post('/', async (req, res) => {
     country: req.body.country,
     phone: req.body.phone,
     status: req.body.status,
-    totalPrice: req.body.totalPrice,
+    totalPrice: totalPrice,
     user: req.body.user
   })
   order = await order.save();
